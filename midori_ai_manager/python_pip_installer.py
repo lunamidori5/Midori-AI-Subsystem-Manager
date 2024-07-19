0471f495e93493fd5253a53a75bed8463abd293d
lunamidori <jaredteam@gmail.com>
2024-07-19 09:21:47 -0700

    fixed bad links

--- midori_ai_manager/python_pip_installer.py
+++ midori_ai_manager/python_pip_installer.py
@@ -1,7 +1,7 @@
 import os
 
 files_to_download = {
-    "requirements.txt": "https://raw.githubusercontent.com/lunamidori5/Midori-AI/master/other_files/model_installer/requirements.txt",
+    "requirements.txt": "https://raw.githubusercontent.com/lunamidori5/Midori-AI-Subsystem-Manager/master/model_installer/requirements.txt",
 }
 
 # Download all the needed files
