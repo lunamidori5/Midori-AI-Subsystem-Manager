6c7c10ad9aa600310a0aefc5ea2b3834be124681
Luna Midori <jaredteam@gmail.com>
2024-09-13 21:59:31 -0700

    Moving more of our stuff on to github

--- subsystem_pixelarch/main.py
+++ subsystem_pixelarch/main.py
@@ -35,37 +35,48 @@ async def subsystem_update():
     n = ui.notification(timeout=None)
     n.message = f'Starting Install... Please wait...'
     n.spinner = True
-    await asyncio.sleep(5)
-    command_str = "docker run -v /var/run/docker.sock:/var/run/docker.sock lunamidori5/pixelarch:subsystem /usr/bin/yay -Syu --noconfirm && echo hello"
-    n.message = f'Running: {command_str}'
-    command_list = command_str.split()
 
-    process = await asyncio.create_subprocess_exec(
-        *command_list,
-        stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
-    )
+    image_download = "lunamidori5/midori_ai_subsystem"
+    image_name = "midori_ai_subsystem_pixelarch"
+    full_image_name = f"--name {image_name}"
+    docker_sock = "-v /var/run/docker.sock:/var/run/docker.sock"
 
-    # Read the output of the process in real time
-    while True:
-        # Read the stdout and stderr of the process
-        stdout, stderr = await process.communicate()
+    command_pre_list = [
+        f"docker pull {image_download}",
+        f"docker run -d {docker_sock} {full_image_name} {image_download} sleep infinity",
+        f"docker exec {image_name} /usr/bin/yay -Syu --noconfirm"
+        ]
 
-        # Print the output to the console
-        output = stdout.decode('utf-8')
-        n.message = f"Output: {output}"
-        command_outerr = stderr.decode('utf-8')
+    for command_str in command_pre_list:
+        n.message = f'Running: {command_str}'
+        command_list = command_str.split()
 
-        # Check if the process has finished running
-        if process.returncode is not None:
-            break
+        process = await asyncio.create_subprocess_exec(
+            *command_list,
+            stdout=asyncio.subprocess.PIPE, stderr=asyncio.subprocess.PIPE
+        )
+
+        # Read the output of the process in real time
+        while True:
+            # Read the stdout and stderr of the process
+            stdout, stderr = await process.communicate()
+
+            # Print the output to the console
+            output = stdout.decode('utf-8')
+            n.message = f"Output: {output}"
+            command_outerr = stderr.decode('utf-8')
+
+            # Check if the process has finished running
+            if process.returncode is not None:
+                break
 
     # Wait for the process to finish running
     await asyncio.sleep(5)
 
-    n.message = 'Done!'
+    n.message = 'Done! Subsystem full installed and updated!'
     n.spinner = False
     markdown_box.update()
-    await asyncio.sleep(2)
+    await asyncio.sleep(25)
 
     n.dismiss()
 
@@ -83,13 +94,14 @@ with ui.row():
     ui.button('Light', on_click=dark.disable)
 
 with ui.row().bind_visibility_from(v, 'value'):
+    markdown_box.update
     with ui.column():
         ui.label("Subsystem Actions:")
         ui.button("1 - Midori AI Subsystem Repair", on_click=subsystem_update)
-        ui.button("2 - Install Backends to Subsystem", on_click=on_button_click)
-        ui.button("3 - Update Backends in Subsystem", on_click=on_button_click)
-        ui.button("4 - Uninstall Backends from Subsystem", on_click=on_button_click)
-        ui.button("5 - Backend Programs (install models / edit backends)", on_click=on_button_click)
-        ui.button("6 - Subsystem and Backend News", on_click=on_button_click)
+        #ui.button("2 - Install Backends to Subsystem", on_click=on_button_click)
+        #ui.button("3 - Update Backends in Subsystem", on_click=on_button_click)
+        #ui.button("4 - Uninstall Backends from Subsystem", on_click=on_button_click)
+        #ui.button("5 - Backend Programs (install models / edit backends)", on_click=on_button_click)
+        #ui.button("6 - Subsystem and Backend News", on_click=on_button_click)
 
 ui.run()
